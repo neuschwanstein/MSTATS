fc5dbcf078ba150d4cf3ee36bc60d9b4d4e91485
Thierry BM <beggars.banquet@gmail.com>
2015-10-15 19:53:54 -0400

    hedging function now complete.

--- Devoir2/Code/blsprice.m
+++ Devoir2/Code/blsprice.m
@@ -1,8 +1,7 @@
-function price = blsprice(S,K,r,T,sigma)
+function [call, put] = blsprice(S,K,r,T,sigma)
 
 d1 = 1/(sigma*sqrt(T)) * (log(S/K) + r*T + sigma^2*T/2);
 d2 = 1/(sigma*sqrt(T)) * (log(S/K) + r*T - sigma^2*T/2);
 call = S.*normcdf(d1) - K*exp(-r*T)*normcdf(d2);
 put = call - S + K*exp(-r*T);
-price = [call, put];
 end
--- Devoir2/Code/hedging.m
+++ Devoir2/Code/hedging.m
@@ -1,12 +1,15 @@
 function V = hedging(S,K,r,T,mu,sigma,put)
-n = length(S);
-beta = exp(-r*linspace(0,T,n));
-S = beta.*S;
-Delta = S(2:end) - S(1:end-1);
-T = linspace(T,0,n);
-phi = blsdelta(S(1:end-1),K,r,T(1:end-1),sigma);
-V0 = blsprice(S(1),K,r,T(1),sigma);
+n = length(S);                  % S: nxd
+beta = diag(exp(-r*linspace(0,T,n))); % beta: nxn
+S = beta*S;                     % Discounted prices.
+Delta = S(2:end,:) - S(1:end-1,:);
+T = linspace(T,0,n)';
+[phiCall, phiPut] = blsdelta(S(1:end-1,:),K,r,T(1:end-1,:),sigma);
+[V0Call, V0Put] = blsprice(S(1),K,r,T(1),sigma);
 
-V = V0' + phi'*Delta;           % Illegal hack: V0=[V0Call,V0Put], same for phi
-V = V(put + 1);                 % Illegal hack !!
+if ~put
+    V = V0Call + diag(phiCall'*Delta);
+else
+    V = V0Put + diag(phiPut'*Delta);
+end
 end
